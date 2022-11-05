8528c7bb305867a7eaabd60479e8c8f60a91266f
sourClout <112970132+sourClout@users.noreply.github.com>
2022-11-05 14:47:27 -0400

    Images Working

---
+++ Pages/NewCourtSuccess.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MTLcourts.Pages
+{
+    public class NewCourtSuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
