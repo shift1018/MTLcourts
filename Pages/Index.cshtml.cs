59fcc8073fbbe9c4fd21baee06404908b60bacd1
sourClout <112970132+sourClout@users.noreply.github.com>
2022-11-07 12:47:36 -0500

    alex image final

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,49 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using MTLcourts.Data;
-
 using MTLcourts.Models;
-
 using Microsoft.EntityFrameworkCore;
-
-
 
 namespace MTLcourts.Pages;
 
-
-
 public class IndexModel : PageModel
-
 {
-
     private readonly ILogger<IndexModel> _logger;
-
-
 
      private readonly CourtsDbContext db;
 
-
-
     public IndexModel(ILogger<IndexModel> logger, CourtsDbContext db)
-
     {
-
         _logger = logger;
-
         this.db = db;
-
     }
-
     public List<Courts> courtsList { get; set; }
 
-
-
     public async Task OnGetAsync()
-
    {
-
     courtsList = await db.Court.ToListAsync();
-
    }
-
+   
 }
