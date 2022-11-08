f8f36d29d4047c4db61b8ead6aa69f1e83262957
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-11-08 09:42:36 -0500

    needtofixjointquery

--- Pages/ViewCourt.cshtml.cs
+++ Pages/ViewCourt.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using MTLcourts.Data;
 using MTLcourts.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace MTLcourts.Pages
 {
@@ -20,38 +21,53 @@ namespace MTLcourts.Pages
 
         [BindProperty(SupportsGet = true)]
         public int Id { get; set; }
-
+     
         public Courts court { get; set; }
 
-        // [BindProperty]
-        // public Comments comment { get; set; }
+        
+        public Comments comment { get; set; }
 
         public List<Comments> courtComments { get; set; }   
 
         public List<Ratings> courtRating { get; set; } 
 
+
+        [BindProperty]
+        public string Comment { get; set; }
+
+        [BindProperty]
+        public int Rating { get; set; }
+
         public async Task OnGetAsync()
         {
             court = await db.Court.Include(court => court.User).Where(court => court.Id == Id).FirstOrDefaultAsync();
             courtComments = await db.Comments.Where(comment => comment.CourtsId == Id).ToListAsync();
+
+           // calculate average rating 
+           
+            // var ratingsQuery = db.Ratings.Where(rating => rating.CourtsId == Id).ToListAsync();
+            // //courtRatingArr[] =
+            // double average = ratingsQuery.Average();
         }
 
         // public async Task OnGetAsync()
         // {
         //     court = await db.Court.Include(court => court.User).Where(court => court.Id == Id).FirstOrDefaultAsync();
-        //     //courtComments = await db.Comments.Where(comment => comment.CourtsId == Id).ToListAsync();
-        //     courtComments = await(from c in db.Comments join u in db.Users on c.User.Id equals u.Id 
-        //                     select new
-        //                     {
-        //                         queryComment = c.Comment,
+        //     courtComments = await db.Comments.Where(comment => comment.CourtsId == Id).ToListAsync();
+
+        //     var queryCourtComments = await(
+        //         from c in db.Comments.Where(comment => comment.CourtsId == Id) 
+        //         join u in db.Users 
+        //         on c.User.Id equals u.Id 
+        //             select new 
+        //             {
+        //                 queryComment = c.Comment,
                                 
-        //                     });
+        //             });
         // }
 
 
 
-        [BindProperty]
-        public string Comment { get; set; }
 
         public async Task<IActionResult> OnPostAsync()
         {
@@ -69,7 +85,19 @@ namespace MTLcourts.Pages
                 DateWhen = DateTime.Now,                
                 Comment = Comment,
             };
+
+            var newRating = new MTLcourts.Models.Ratings{
+                CourtsId = courtsId,
+                User = user,
+                Rating = Rating
+            };
+
+ 
+
+
+
             db.Comments.Add(newComment);
+            db.Ratings.Add(newRating);
             await db.SaveChangesAsync();
 
             return RedirectToPage("ViewCourt", Id);
