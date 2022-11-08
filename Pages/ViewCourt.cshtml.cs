64bebcf65e142923a4a5bab9cce377ed98fe4f92
AntonDiachenko <99092120+AntonDiachenko@users.noreply.github.com>
2022-11-08 13:44:07 -0500

    count average

--- Pages/ViewCourt.cshtml.cs
+++ Pages/ViewCourt.cshtml.cs
@@ -38,16 +38,15 @@ namespace MTLcourts.Pages
         [BindProperty]
         public int Rating { get; set; }
 
+        [BindProperty]
+        public double AvgRating { get; set; }
+
         public async Task OnGetAsync()
         {
             court = await db.Court.Include(court => court.User).Where(court => court.Id == Id).FirstOrDefaultAsync();
-            courtComments = await db.Comments.Where(comment => comment.CourtsId == Id).ToListAsync();
+            courtComments = await db.Comments.Include(comment => comment.User).Where(comment => comment.CourtsId == Id).ToListAsync();
 
-           // calculate average rating 
-           
-            // var ratingsQuery = db.Ratings.Where(rating => rating.CourtsId == Id).ToListAsync();
-            // //courtRatingArr[] =
-            // double average = ratingsQuery.Average();
+
         }
 
         // public async Task OnGetAsync()
@@ -92,15 +91,62 @@ namespace MTLcourts.Pages
                 Rating = Rating
             };
 
- 
+           //calculate average rating 
+// !!!!!!!!!!! need to fix the query - giving an error
+            // var findUser = await db.Ratings.Where(r => r.CourtsId == Id && r.User.UserName == userName).ToListAsync();
+            // if (findUser != null){
+            //     ModelState.AddModelError(string.Empty, "You already rated this court in the past.");
+            //     return Page();
+            // }else{
+            // var ratingsQuery = await db.Ratings.Where(rating => rating.CourtsId == Id).ToListAsync();
+            // var count = 0;
+            // var sum = 0;
+            // foreach (var rat in ratingsQuery)
+            // {
+            //     sum += rat.Rating;
+            //     count++;
+            // }
 
+            // double AvgRatingNew = Math.Round((double)(sum+Rating)/(double)(count+1), 2); 
+            // court = await db.Court.Where(court => court.Id == Id).FirstOrDefaultAsync();
+            // court.AvgRating = AvgRatingNew;
+
+
+            // db.Comments.Add(newComment);
+            // db.Ratings.Add(newRating);
+            // //db.Court.Update(court.AvgRating = AvgRating1).Where(court => court.Id == Id);
+            // await db.SaveChangesAsync();
+
+            // return RedirectToPage("ViewCourt", Id);                
+            // }
+
+            var findUser = await db.Ratings.Where(r => r.CourtsId == Id && r.User.UserName == userName).ToListAsync();
+            if (findUser != null){
+                ModelState.AddModelError(string.Empty, "You already rated this court in the past.");
+                return Page();
+            }else{
+            var ratingsQuery = await db.Ratings.Where(rating => rating.CourtsId == Id).ToListAsync();
+            var count = 0;
+            var sum = 0;
+            foreach (var rat in ratingsQuery)
+            {
+                sum += rat.Rating;
+                count++;
+            }
+
+            double AvgRatingNew = Math.Round((double)(sum+Rating)/(double)(count+1), 2); 
+            court = await db.Court.Where(court => court.Id == Id).FirstOrDefaultAsync();
+            court.AvgRating = AvgRatingNew;
 
 
             db.Comments.Add(newComment);
             db.Ratings.Add(newRating);
+            //db.Court.Update(court.AvgRating = AvgRating1).Where(court => court.Id == Id);
             await db.SaveChangesAsync();
 
-            return RedirectToPage("ViewCourt", Id);
+            return RedirectToPage("ViewCourt", Id);                
+            }
+
 
         }
 
